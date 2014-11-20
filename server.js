b677c9a7913f655edebc1cb410d5a3b2edcfd954
TristanDarwent <tristandarwent@gmail.com>
2014-11-20 12:33:51 -0500

    Added z-index changing
    Changes z-index to be above static planets

--- server.js
+++ server.js
@@ -32,33 +32,33 @@ io.on('connection', function(socket) {
   console.log('A User Connected');
 
   // Sets positions of planets to newly connected users with coordinates stored in array in server, also hiding the label for each
-  io.emit('update', 'mercury', coordinates.mercury[0], coordinates.mercury[1]);
-  io.emit('updateLabel', 'mercury');
-  io.emit('update', 'venus', coordinates.venus[0], coordinates.venus[1]);
-  io.emit('updateLabel', 'venus');
-  io.emit('update', 'earth', coordinates.earth[0], coordinates.earth[1]);
-  io.emit('updateLabel', 'earth');
-  io.emit('update', 'mars', coordinates.mars[0], coordinates.mars[1]);
-  io.emit('updateLabel', 'mars');
-  io.emit('update', 'jupiter', coordinates.jupiter[0], coordinates.jupiter[1]);
-  io.emit('updateLabel', 'jupiter');
-  io.emit('update', 'saturn', coordinates.saturn[0], coordinates.saturn[1]);
-  io.emit('updateLabel', 'saturn');
-  io.emit('update', 'uranus', coordinates.uranus[0], coordinates.uranus[1]);
-  io.emit('updateLabel', 'uranus');
-  io.emit('update', 'neptune', coordinates.neptune[0], coordinates.neptune[1]);
-  io.emit('updateLabel', 'neptune');
+  io.emit('movePlanet', 'mercury', coordinates.mercury[0], coordinates.mercury[1]);
+  io.emit('removePlanet', 'mercury');
+  io.emit('movePlanet', 'venus', coordinates.venus[0], coordinates.venus[1]);
+  io.emit('removePlanet', 'venus');
+  io.emit('movePlanet', 'earth', coordinates.earth[0], coordinates.earth[1]);
+  io.emit('removePlanet', 'earth');
+  io.emit('movePlanet', 'mars', coordinates.mars[0], coordinates.mars[1]);
+  io.emit('removePlanet', 'mars');
+  io.emit('movePlanet', 'jupiter', coordinates.jupiter[0], coordinates.jupiter[1]);
+  io.emit('removePlanet', 'jupiter');
+  io.emit('movePlanet', 'saturn', coordinates.saturn[0], coordinates.saturn[1]);
+  io.emit('removePlanet', 'saturn');
+  io.emit('movePlanet', 'uranus', coordinates.uranus[0], coordinates.uranus[1]);
+  io.emit('removePlanet', 'uranus');
+  io.emit('movePlanet', 'neptune', coordinates.neptune[0], coordinates.neptune[1]);
+  io.emit('removePlanet', 'neptune');
 
   // Sends planet position changes main.js
-  socket.on('moveShape', function(id, x, y) {
-    io.emit('update', id, x, y);
+  socket.on('move', function(id, x, y) {
+    io.emit('movePlanet', id, x, y);
     coordinates[id][0] = x;
     coordinates[id][1] = y;
   });
 
-  // Sends label id to hide to main.js
-  socket.on('removeLabel', function(id) {
-    io.emit('updateLabel', id);
+  // Sends planet id to hide label and reset z-index to main.js
+  socket.on('remove', function(id) {
+    io.emit('removePlanet', id);
   });
 });
 
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -42,7 +42,7 @@ jQuery(function($) {
             planetId = $(this).attr('id');
 
             // Emits id and coordinates of planet while being moved
-            socket.emit('moveShape', planetId, newX, newY);
+            socket.emit('move', planetId, newX, newY);
         });
     });
     
@@ -51,20 +51,22 @@ jQuery(function($) {
         $('.planet').unbind('mousemove');
 
         // Send to server the id of planet that has been released, to hide label
-        socket.emit('removeLabel', planetId);
+        socket.emit('remove', planetId);
     });
 
-    // Updates planets with new positions through sockets
-    socket.on('update', function(id, x, y) {
+    // Updates planets with new positions and z-index through sockets
+    socket.on('movePlanet', function(id, x, y) {
         $('#' + id).css('left', x);
         $('#' + id).css('top', y);
+        $('#' + id).css('z-index', 99);
 
         // Shows label of currently moving planet
         $('.' + id + 'Label').css('opacity', 1);
     });
 
-    // Hides label of planet after it has stopped moving
-    socket.on('updateLabel', function(id) {
+    // Hides label of planet and resets z-index after it has stopped moving
+    socket.on('removePlanet', function(id) {
         $('.' + id + 'Label').css('opacity', 0);
+        $('#' + id).css('z-index', 0);
     });
 });
